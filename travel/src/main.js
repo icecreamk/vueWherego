ee4e51e381af5620010fcd505d37103f24d127fa
e <550893685@qq.com>
2021-11-14 09:26:00 +0800

    fix:解决移动端300ms问题

--- travel/src/main.js
+++ travel/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import fastClick from 'fastclick'
 import '../src/assets/styles/reset.css'
 import '../src/assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
